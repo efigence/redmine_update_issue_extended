8a00860f66fdf7cbb6ab94f292d1e66805561bd2
Jacek Grzybowski <jgrzybowski@artegence.com>
2013-10-30 16:56:50 +0100

    renamed setting view

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_update_issue_extended do
   url 'http://github.com/efigence/redmine_update_issue_extended'
   author_url 'http://efigence.com'
 
-  settings :default => {}, :partial => 'settings/settings'
+  settings :default => {}, :partial => 'settings/update_issue_extended_settings'
 end
 
 require 'redmine_update_issue_extended/hooks/view_issues_edit_hook'
