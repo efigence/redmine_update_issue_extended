af63258700649746afffeb37d09d9f40c8e94546
Jacek Grzybowski <jgrzybowski@artegence.com>
2014-02-06 14:24:09 +0100

    preview button bug fix

--- assets/javascripts/extend_description.js
+++ assets/javascripts/extend_description.js
@@ -1,6 +1,9 @@
 $(function(){
   $(document).bind('ajaxSuccess', function() {
-    desc_caption = $('#hidden_edit_desc').text()
-    $("label[for='issue_description']").next().append(desc_caption);
+    desc_caption = $('#hidden_edit_desc').text();
+    edit_link = $("label[for='issue_description']").next();
+    if(edit_link.text().length == 0) {
+      edit_link.append(desc_caption);
+    };
   });
 });
