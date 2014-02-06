f8dd682d6ec7831857d7683a1aef75c32cdccb75
Jacek Grzybowski <jgrzybowski@artegence.com>
2014-02-06 14:35:31 +0100

    cleanup

--- assets/javascripts/extend_description.js
+++ assets/javascripts/extend_description.js
@@ -2,7 +2,7 @@ $(function(){
   $(document).bind('ajaxSuccess', function() {
     desc_caption = $('#hidden_edit_desc').text();
     edit_link = $("label[for='issue_description']").next();
-    if(edit_link.text().length == 0) {
+    if(!edit_link.text().length) {
       edit_link.append(desc_caption);
     };
   });
